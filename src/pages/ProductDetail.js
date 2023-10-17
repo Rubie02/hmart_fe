0f69cc68ec2d0ea921ebc2cd21733fd230061e75
Rubie02 <hoainn9a@gmail.com>
2023-10-17 11:46:30 +0700

    product detail page

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -4,10 +4,22 @@ import Meta from '../components/Meta'
 import ProductCard from '../components/ProductCard'
 import ReactStars from "react-rating-stars-component"
 import ReactImageZoom from "react-image-zoom"
+import Color from '../components/Color'
+import { TbGitCompare } from 'react-icons/tb'
+import { AiOutlineHeart } from 'react-icons/ai'
+// import { Link } from 'react-router-dom'
 
 const ProductDetail = () => {
   const props = { width: 600, height: 500, zoomWidth: 500, img: "/images/product-details-1.jpg"}
   const [orderedProduct, setOrderProduct] = useState(true);
+  const copyToClipboard = (text) => {
+    var textField = document.createElement("textarea");
+    textField.innerText = text;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
+  }
   return (
     <>
       <Meta title={"HMart | Product Detail"}/>
@@ -54,22 +66,85 @@ const ProductDetail = () => {
                             </div>
                             <a className='review-btn text-decoration-underline' href='#review'>Write a Review</a>
                         </div>
-                        <div className='border-buttom py-3'>
-                            <div className='d-flex gap-10 align-items-center'>
+                        <div className='py-3'>
+                            <div className='d-flex gap-10 align-items-center my-2'>
                                 <h3 className='product-heading'>Type: </h3>
-                                <p className='product-data'>GDF</p>
+                                <p className='product-data'>Smart</p>
                             </div>
-                            <div className='d-flex gap-10 align-items-center'>
+                            <div className='d-flex gap-10 align-items-center my-2'>
                                 <h3 className='product-heading'>Brand: </h3>
-                                <p className='product-data'>GDF</p>
+                                <p className='product-data'>Havels</p>
                             </div>
-                            <div className='d-flex gap-10 align-items-center'>
+                            <div className='d-flex gap-10 align-items-center my-2'>
                                 <h3 className='product-heading'>Category: </h3>
-                                <p className='product-data'>GDF</p>
+                                <p className='product-data'>Watch</p>
                             </div>
-                            <div className='d-flex gap-10 align-items-center'>
+                            <div className='d-flex gap-10 align-items-center my-2'>
                                 <h3 className='product-heading'>Tags: </h3>
-                                <p className='product-data'>GDF</p>
+                                <p className='product-data'>Watch</p>
+                            </div>
+                            <div className='d-flex gap-10 align-items-center my-2'>
+                                <h3 className='product-heading'>Availbility: </h3>
+                                <p className='product-data'>In Stock</p>
+                            </div>
+                            <div className='d-flex gap-10 flex-column mt-2 mb-3'>
+                                <h3 className='product-heading'>Size: </h3>
+                                <div className='d-flex flex-wrap gap-10'>
+                                    <span className='badge border border-1 bg-white text-dark border-secondary'>S</span>
+                                    <span className='badge border border-1 bg-white text-dark border-secondary'>M</span>
+                                    <span className='badge border border-1 bg-white text-dark border-secondary'>L</span>
+                                    <span className='badge border border-1 bg-white text-dark border-secondary'>XL</span>
+                                    <span className='badge border border-1 bg-white text-dark border-secondary'>XXL</span>
+                                </div>
+                            </div>
+                            <div className='d-flex gap-10 flex-column mt-2 mb-3'>
+                                <h3 className='product-heading'>Color: </h3>
+                                <Color />
+                            </div>
+                            <div className='d-flex align-items-center gap-15 flex-row mt-2 mb-3'>
+                                <h3 className='product-heading'>Quantity: </h3>
+                                <div className=''>
+                                    <input 
+                                        className='form-control'
+                                        type='number'
+                                        defaultValue={1}
+                                        min={1}
+                                        max={10}
+                                        style={{width: "60px"}}
+                                    />
+                                </div>
+                                <div className='d-flex align-items-center gap-30 ms-5'>
+                                    <button className='submit-button border-0' type='submit'>
+                                        Add to Cart
+                                    </button>
+                                    <button to='' className='submit-button signup'>
+                                        Buy It Now
+                                    </button>
+                                </div>
+                                
+                            </div>
+                            <div className='d-flex align-items-center gap-15'>
+                                    <div>
+                                        <a href='/compare-product'>
+                                        <TbGitCompare className='fs-5 me-2' />   Add to Compare</a>
+                                    </div>
+                                    <div>
+                                        <a href='/wishlist'>
+                                        <AiOutlineHeart className='fs-5 me-2' />Add to WishList</a>
+                                    </div>
+                            </div>
+                            <div className='d-flex gap-10 flex-column my-3'>
+                                <h3 className='product-heading'>Shipping & Returns: </h3>
+                                <p className='product-data'>
+                                    Free shipping and returns available on all orders! <br />
+                                    We shipp all Vietnam domestic orders within <b>3-7 business days!</b>
+                                </p>
+                            </div>
+                            <div className='d-flex gap-10 align-items-center my-3'>
+                                <h3 className='product-heading'>Product Link: </h3>
+                                <a href='javascript:void(0)' onClick={() => {copyToClipboard("localhost")}}>
+                                    Copy Product Link
+                                </a>
                             </div>
                         </div>
                     </div>
